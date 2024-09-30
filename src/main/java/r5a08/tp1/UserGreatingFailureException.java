6605f450973f41614b55b3a723d959ba78bf9aa4
Pauline-Vu <paulinevu.pro@gmail.com>
2024-09-30 17:11:46 +0200

    test

--- src/main/java/r5a08/tp1/UserGreatingFailureException.java
+++
@@ -1,7 +0,0 @@
-package r5a08.tp1;
-
-public class UserGreatingFailureException extends RuntimeException {
-    public UserGreatingFailureException(String message){
-        super(message);
-    }
-}
